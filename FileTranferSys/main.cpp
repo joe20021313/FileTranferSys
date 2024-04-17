407f46cd9a1cf734a1df96cd6c890688a56b040f
Joe <131233284+joe20021313@users.noreply.github.com>
2024-04-17 14:23:50 +0100

    created a socket connection

--- FileTranferSys/main.cpp
+++ FileTranferSys/main.cpp
@@ -1,8 +1,46 @@
-#include "main.h"
 #include <iostream>
 #include <asio.hpp>
+#ifdef  _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif //  _WIN32
+
+#define ASIO_STANDALONE
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hellffrrfdo, wordd!" << std::endl;
+    
+    asio::io_context context; // unique instance of asio that has all the requirements for our application
+    asio::error_code ec;
+
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.32", ec), 80);
+
+    asio::ip::tcp::socket socket(context);
+
+
+    socket.connect(endpoint, ec);
+
+    if (!ec) {
+
+       
+        std::cout << "Connected connect \n"  << std::endl;
+           
+       
+
+    }
+    else {
+
+        std::cout << "Failed to connect \n" << ec.message() << std::endl;
+    }
+
+    if (socket.is_open()) {
+
+        
+    }
+
     return 0;
+
+
 }
